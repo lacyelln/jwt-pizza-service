3a23b71e438b33a2f377f2204c3acbb4a1bb1b19
lacyelln <lacyellenmiller@gmail.com>
2025-09-30 18:23:22 -0600

    i am so confused

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,20 +1,14 @@
+/* eslint-env jest */
 const request = require('supertest');
 const app = require('../service');
-// const { expectValidJwt, createAdminUser } = require('./testUtils');
+const { expectValidJwt, createAdminUser } = require('./testUtils');
+
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
-
-const { Role, DB } = require('../database/database.js');
-
-async function createAdminUser() {
-  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-  user.name = randomName();
-  user.email = user.name + '@admin.com';
-
-  user = await DB.addUser(user);
-  return { ...user, password: 'toomanysecrets' };
-}
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
@@ -23,48 +17,51 @@ beforeAll(async () => {
   expectValidJwt(testUserAuthToken);
 });
 
-test('register success', async() => {
-    const registerRes = await request(app).post('/api/auth').send(testUser);
-    expect(registerRes.status).toBe(200);
-    expect(registerRes.body.user.name).toBe('pizza diner');
-    expectValidJwt(registerRes.body.token);
+test('bad register', async () => {
+  const badUser = { name: 'pizza diner', email: 'reg@test.com'};
+  const res = await request(app).post('/api/auth').send(badUser);
+  expect(res.statusCode).toBe(400);
+  expect(res.body.message).toBe('name, email, and password are required');
 });
 
-test('login success', async () => {
+test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
   expectValidJwt(loginRes.body.token);
 
-  const expectedUser = { ...testUser, roles: [] };
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
   delete expectedUser.password;
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
 
-test('logout unauthorized', async () => {
-    const res = await request(app).delete('/api/auth');
-    expect(res.status).toBe(401);
+test('register', async () => {
+  const user = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+  user.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(user);
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
+})
 
+
+test('logout', async () => {
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
 });
 
-test('register unsuccessful', async () => {
-    const res = await request(app).post('/api/auth').send({email: "no@test.com"});
-    expect(res.status).toBe(400);
-    expect(res.body.message).toBe('name, email, and password are required');
-});
+test('update user', async () => {
+  const adminUser = await createAdminUser();
 
-test('logout successful', async() => {
-    const res = await request(app).delete('/api/auth').set('Authorization', `Bearer ${testUserAuthToken}`);
+  const loginRes = await request(app)
+    .put('/api/auth')
+    .send({ email: adminUser.email, password: 'toomanysecrets' });
+
+  const adminToken = loginRes.body.token;
+  const user = { email: 'new_email@jwt.com', password: 'new_password' };
+
+  const res = await request(app).put(`/api/auth/${adminUser.id}`).set('Authorization', `Bearer ${
+    adminToken}`).send(user);
+
     expect(res.status).toBe(200);
-    expect(res.body.message).toBe('logout successful');
+    expect(res.body).toMatchObject({ email: user.email, });
 });
-
-
-function expectValidJwt(potentialJwt) {
-  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-}
-
-function randomName() {
-  return Math.random().toString(36).substring(2, 12);
-}
-
-
