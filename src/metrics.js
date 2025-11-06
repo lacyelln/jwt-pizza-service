db436417e0883b14650ec841ae508c2ffebb2ffe
lacyelln <lacyellenmiller@gmail.com>
2025-11-06 12:25:32 -0700

    updated to call the metrics api and url

--- src/metrics.js
+++ src/metrics.js
@@ -46,10 +46,10 @@ function sendMetricToGrafana(metricName, metricValue, type, unit) {
   }
 
   const body = JSON.stringify(metric);
-  fetch(`${config.url}`, {
+  fetch(`${config.metrics.url}`, {
     method: 'POST',
     body: body,
-    headers: { Authorization: `Bearer ${config.apiKey}`, 'Content-Type': 'application/json' },
+    headers: { Authorization: `Bearer ${config.metrics.apiKey}`, 'Content-Type': 'application/json' },
   })
     .then((response) => {
       if (!response.ok) {
